8db11c07cc9acc07729b58beaa4ff0b77e73f3f3
gcsilva <gc.silva@gmail.com>
2022-02-10 17:41:05 -0300

    .

--- backend/init.sh
+++ backend/init.sh
@@ -9,7 +9,8 @@ ls -la
 # /var/www/vendor/laravel/sail/bin/sail up -d
 # chmod 777 composer.lock /var/www/vendor/laravel/sail/bin/sail
 # rm -f composer.lock /var/www/vendor/laravel/sail/bin/sail
-php /usr/local/bin/composer install --no-scripts --no-autoloader
+php /usr/local/bin/composer update --no-scripts --no-autoloader
+composer dump-autoload
 chmod 777 -R .
 php artisan migrate --path=/database/migrations/2022_02_04_033813_create_cursos_table.php
 php artisan db:seed CursoSeeder
