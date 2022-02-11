55cc5b469122f050634510075684fef88ed63a11
gcsilva <gc.silva@gmail.com>
2022-02-11 10:59:01 -0300

    last change

--- backend/init.sh
+++ backend/init.sh
@@ -10,8 +10,7 @@ ls -la
 # chmod 777 composer.lock /var/www/vendor/laravel/sail/bin/sail
 # rm -f composer.lock /var/www/vendor/laravel/sail/bin/sail
 php /usr/local/bin/composer install --no-scripts --no-autoloader
-chmod 777 -R .
-php artisan migrate --path=/database/migrations/2022_02_04_033813_create_cursos_table.php
-php artisan db:seed CursoSeeder
+php artisan migrate --path=/database/migrations/2022_02_04_033813_create_cursos_table.php --force
+php artisan db:seed CursoSeeder --force
 
 # php-fpm
